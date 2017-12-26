322a531a65030cd865de169728a8832088f680f0
december <luyunfeicx@163.com>
2017-12-26 21:50:31 +0800

    data processing

---
+++ DataProcessing.py
@@ -0,0 +1,108 @@
+import os
+
+infodic = {} #form tweet id to the info of it(user_id, tweet_time, reply_tweet_id, reply_user_id, root_tweet_id, root_user_id)
+cascade = {} #from root id to the info of tweets in cascade(tweet_id, user_id, tweet_time, reply_tweet_id, reply_user_id)
+tweetdic = {} #from real tweet id to our tweet id
+userdic = {} #from real user id to our user id
+prefix = '../../dataset/tencent_weibo_2011_11/'
+namelist = os.listdir(prefix)
+tweet_cnt = 0
+user_cnt = 0
+for name in namelist:
+	if name.startswith('2011_'):
+		fr = open(prefix+name, 'r')
+		data = fr.readlines()
+		fr.close()
+		n = len(data)
+		while i < n:
+			if i + 12 >= n:
+				break
+			tid = data[i+1][2:-1]
+			tm = data[i+2][2:-1]
+			tu = data[i+3][2:-1]
+			rt = data[i+9][2:-1]
+			ru = data[i+10][2:-1]
+			pt = data[i+11][2:-1]
+			pu = data[i+12][2:-1]
+			if tweetdic.has_key(tid):
+				tid = tweetdic[tid]
+			else:
+				tweetdic[tid] = tweet_cnt
+				tid = tweet_cnt
+				tweet_cnt += 1
+			if userdic.has_key(tu):
+				tu = userdic[tu]
+			else:
+				userdic[tu] = user_cnt
+				tu = user_cnt
+				user_cnt += 1
+
+			if int(pt) != 0 and int(pu) != 0:
+				if tweetdic.has_key(rt):
+					rt = tweetdic[rt]
+				else:
+					tweetdic[rt] = tweet_cnt
+					rt = tweet_cnt
+					tweet_cnt += 1
+				if tweetdic.has_key(pt):
+					pt = tweetdic[pt]
+				else:
+					tweetdic[pt] = tweet_cnt
+					pt = tweet_cnt
+					tweet_cnt += 1
+				if userdic.has_key(ru):
+					ru = userdic[ru]
+				else:
+					userdic[ru] = user_cnt
+					ru = user_cnt
+					user_cnt += 1
+				if userdic.has_key(pu):
+					pu = userdic[pu]
+				else:
+					userdic[pu] = user_cnt
+					pu = user_cnt
+					user_cnt += 1
+			else:
+				pt = -1
+				pu = -1
+				rt = tid
+				ru = tu
+			infodic[tid] = list()
+			infodic[tid].append(tu)
+			infodic[tid].append(int(tm))
+			infodic[tid].append(pt)
+			infodic[tid].append(pu)			
+			infodic[tid].append(rt)
+			infodic[tid].append(ru)
+			i += 21
+
+infolist = sorted(infodic.items(), key=lambda d:d[1][1])
+for info in infolist:
+	if info[1][2] == -1:
+		cascade[info[0]] = list()
+		temp = list()
+		temp.append(info[0])
+		temp.extend(info[1][:4])
+		cascade[info[0]].append(temp)
+	else:
+		if not cascade.has_key(info[1][4]):
+			continue
+		temp = list()
+		temp.append(info[0])
+		temp.extend(info[1][:4])
+		cascade[info[1][4]].append(temp)
+print len(cascade)
+caslist = sorted(cascade.items(), key=lambda d:d[1][0][1])
+fw = open('../../cascading_generation_model/all/all.detail', 'w')
+for cas in caslist:
+	fw.write(str(cas[0]))
+	fw.write('\t')
+	fw.write(str(len(cas[1])))
+	fw.write('\n')
+	for tweet in cas[1]:
+		fw.write(str(tweet[0]))
+		for i in range(1, 5):
+			fw.write('\t')
+			fw.write(str(tweet[i]))
+		fw.write('\n')
+fw.close()
--- getWienerDistribution.py
+++ getWienerDistribution.py
@@ -3,6 +3,26 @@ import numpy as np
 import sys
 import os
 
+def WienerIndex(gdic, j, k):
+	wi = 0
+	if gdic[j][1] < gdic[k][1]:
+		while gdic[j][1] < gdic[k][1]:
+			j = gdic[j][0]
+			wi += 1
+		if j == k:
+			return wi
+	if gdic[j][1] > gdic[k][1]:
+		while gdic[j][1] > gdic[k][1]:
+			k = gdic[k][0]
+			wi += 1
+		if j == k:
+			return wi
+	while j != k:
+		j = gdic[j][0]
+		k = gdic[k][0]
+		wi += 2
+	return wi
+		
 filename = int(sys.argv[1])
 if filename < 0:
 	single = False
@@ -28,17 +48,27 @@ while i < n:
 	temp = realdata[i].split('\t')
 	number = int(temp[1]) + 1
 	depdic = {}
+	graphdic = {}
+	keylist = list()
 	for j in range(i+1, i+number):
 		info = realdata[j].split('\t')
+		keylist.append(info[0])
+		graphdic[info[0]] = list()
+		graphdic[info[0]].append(info[3])
 		if info[3] == '-1':
 			depdic[info[0]] = 0
 		else:
 			depdic[info[0]] = depdic[info[3]] + 1
-	dep = max(depdic.values())
-	if real.has_key(dep):
-		real[dep] += 1
+		graphdic[info[0]].append(depdic[info[0]])
+	wi = 0
+	m = len(keylist)
+	for j in range(m):
+		for k in range(j+1, m):
+			wi += WienerIndex(graphdic, keylist[j], keylist[k])
+	if real.has_key(wi):
+		real[wi] += 1
 	else:
-		real[dep] = 1
+		real[wi] = 1
 	i += number
 
 namelist = os.listdir(prefix+str(filename)+'/')
@@ -52,17 +82,27 @@ for name in namelist:
 		temp = simdata[i].split('\t')
 		number = int(temp[1])
 		depdic = {}
+		graphdic = {}
+		keylist = list()
 		for j in range(i+1, i+number):
 			info = realdata[j].split('\t')
+			keylist.append(info[0])
+			graphdic[info[0]] = list()
+			graphdic[info[0]].append(info[3])
 			if info[3] == '-1':
 				depdic[info[0]] = 0
 			else:
 				depdic[info[0]] = depdic[info[3]] + 1
-		dep = max(depdic.values())
-		if sim.has_key(dep):
-			sim[dep] += 1
+			graphdic[info[0]].append(depdic[info[0]])
+		wi = 0
+		m = len(keylist)
+		for j in range(m):
+			for k in range(j+1, m):
+				wi += WienerIndex(graphdic, keylist[j], keylist[k])
+		if sim.has_key(wi):
+			sim[wi] += 1
 		else:
-			sim[dep] = 1
+			sim[wi] = 1
 		i += number
 	fr.close()
 
@@ -85,8 +125,8 @@ ss = np.array(simsize)
 sn = np.array(simnum) * 1.0 / simsum
 plt.plot(rs, rn, 'ro', label='Real')
 plt.plot(ss, sn, 'bo', label='Sim')
-plt.xlabel(u'Depth')
+plt.xlabel(u'Wiener Index')
 plt.ylabel(u'Distribution')
 plt.legend(loc='upper right');  
-plt.savefig(prefix+'DepthDistribution/'+str(filename)+'.png')
+plt.savefig(prefix+'WienerIndexDistribution/'+str(filename)+'.png')
 plt.cla()
