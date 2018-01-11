951c7f7c23b4e37056635730b149888242835ecc
december <luyunfeicx@163.com>
2018-01-11 09:31:56 +0800

    fix bug

--- getPearson.py
+++ getPearson.py
@@ -156,11 +156,12 @@ while i < n:
 		for key in rtdic:
 			if len(rtdic[key]) <= 1:
 				continue
-			#print rtdic[key]
+			print rtdic[key]
 			keylist = rtdic[key].keys()
 			m = len(keylist)
 			for l in range(m):
 				for k in range(l+1, m):
+					print rtdic[key][keylist[l]] + ' ' + rtdic[key][keylist[k]]
 					rr_real += calcPearson(rtdic[key][keylist[l]], rtdic[key][keylist[k]])
 					rr_rd += chooseTwo(authordic[key])
 					rr_real_sum += 1
