e3812313d5861844ebf4faef0e4bdd63720a9cb3
路云飞 <luyunfeicx@163.com>
2018-01-20 15:42:54 +0800

    use equal

--- EM_tensorflow_new.py
+++ EM_tensorflow_new.py
@@ -412,7 +412,7 @@ theta4 = np.zeros(allusers) #one of spherical coordinates of phi distribution
 
 omega += sum(lbd) * 100 / users
 omega = np.arccos(np.sqrt(omega))
-
+'''
 fr = open(prefix+'lda'+suffix, 'r')
 ldainfo = fr.readlines()
 print 'Use lda result as initial distribution...'
@@ -432,7 +432,7 @@ theta1 += np.arccos(np.sqrt(0.2))
 theta2 += np.arccos(np.sqrt(0.25))
 theta3 += np.arccos(np.sqrt(1.0 / 3))
 theta4 += np.arccos(np.sqrt(0.5))
-
+'''
 tr = list()
 for i in range(4):
 	tr.append(np.random.rand())
