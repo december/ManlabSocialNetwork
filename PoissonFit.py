1a66e6a8872c7699df7633b17103eed4bbf18f75
december <luyunfeicx@163.com>
2018-01-24 00:15:18 +0800

    add print

--- PoissonFit.py
+++ PoissonFit.py
@@ -40,12 +40,12 @@ for i in range(users):
 	posts[i] = int(temp[1])
 fr.close()
 lbd += sum(posts) * 1.0 / 7268 / 86400
-print lbd
+
 for i in range(users):
 	pos = int(posts[i] + 1)
 	for j in range(2, pos):
 		lnorder[i] += np.log(j)
-print lnorder
+#print lnorder
 
 cascades = list()
 for i in range(100):
@@ -67,6 +67,7 @@ while i < n:
 i = 0
 for i in range(users):
 	sum_iet[i] = DeltaSum(ptdic[uid[i]])
+print min(sum_iet)
 
 '''
 #Optimize manually
