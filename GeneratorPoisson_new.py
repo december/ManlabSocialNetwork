890942ca20726dbb9fea3e43805ea424aa6b5421
december <luyunfeicx@163.com>
2018-01-23 22:35:00 +0800

    try x

--- GeneratorPoisson_new.py
+++ GeneratorPoisson_new.py
@@ -160,7 +160,7 @@ for i in range(enum):
 	temp = xlist[i].split('\t')
 	x[i] = float(temp[2])
 fr.close()
-x -= 1.95
+x -= 2
 
 print 'Finished reading..'
 prefix = '../../cascading_generation_model/simulation/'
