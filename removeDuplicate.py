e0228418b4e4992653727c6034a01a548ae6d704
december <luyunfeicx@163.com>
2018-01-25 22:08:09 +0800

    debug

--- removeDuplicate.py
+++ removeDuplicate.py
@@ -73,6 +73,8 @@ for name in namelist:
 						reflectdic[info[3]] = prdic[prdic[info[3]]]
 						reflectdic[info[0]] = prdic[prdic[info[0]]]
 		l = len(tempdata)
+		if temp[0] == '418065':
+			print 'arrive'
 		for j in range(l):
 			info = tempdata[j].split('\t')
 			if reflectdic.has_key(info[3]):
