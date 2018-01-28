54d0c6a0cf58b065c188f8c698c0b9a37a4b813f
december <luyunfeicx@163.com>
2018-01-28 16:12:10 +0800

    fix bug

--- GeneratorPoisson_new.py
+++ GeneratorPoisson_new.py
@@ -166,7 +166,7 @@ for i in range(enum):
 	temp = xlist[i].split('\t')
 	x[i] = float(temp[2])
 fr.close()
-x -= 1.5
+x -= 1.8
 
 print 'Finished reading..'
 prefix = '../../cascading_generation_model/simulation/'
