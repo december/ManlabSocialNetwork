1a8ce0582adc150010f14186c2c2306826e34581
december <luyunfeicx@163.com>
2018-01-28 16:21:02 +0800

    fix bug

--- GeneratorPoisson_new.py
+++ GeneratorPoisson_new.py
@@ -67,7 +67,8 @@ def GetLog(r, p, u, t, tau, c, d): #root_tweet, parent_tweet, parent_user, paren
 		see = t + GetIET(omega[f])
 		if see > te:
 			continue
-		thres = d ** -x[edgemap[u][f]] * pi[edgemap[u][f]] * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)
+		#thres = d ** -x[edgemap[u][f]] * pi[edgemap[u][f]] * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)
+		thres = x[edgemap[u][f]] ** -d * pi[edgemap[u][f]] * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)
 		if np.random.rand() <= thres:
 			current = number
 			tweetdic[current] = f
@@ -166,7 +167,7 @@ for i in range(enum):
 	temp = xlist[i].split('\t')
 	x[i] = float(temp[2])
 fr.close()
-x -= 2.5
+x -= 1
 
 print 'Finished reading..'
 prefix = '../../cascading_generation_model/simulation/'
