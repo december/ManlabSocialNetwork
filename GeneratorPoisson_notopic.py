bcc756f796cbbbcc673afa891d3cbec763f62924
december <luyunfeicx@163.com>
2018-01-30 20:02:06 +0800

    fix bug

--- GeneratorPoisson_notopic.py
+++ GeneratorPoisson_notopic.py
@@ -142,7 +142,7 @@ for i in range(enum):
 	temp = xlist[i].split('\t')
 	x[i] = float(temp[2])
 fr.close()
-x -= 0.1
+x -= 0.01
 
 print 'Finished reading..'
 prefix = '../../cascading_generation_model/simulation_notopic/'
