90b612b1cc446a883d5cb8c92d7eea71beb99634
december <luyunfeicx@163.com>
2018-01-30 13:03:15 +0800

    fix bug

--- getPolarityDistribution.py
+++ getPolarityDistribution.py
@@ -150,10 +150,10 @@ plt.xscale('log')
 plt.yscale('log')
 plt.plot(rs, rn, 'ro', label='Real')
 plt.plot(ss, sn, 'bo', label='Sim')
-plt.xlabel(u'Wiener Index')
+plt.xlabel(u'Polarity')
 plt.ylabel(u'Distribution')
 plt.legend(loc='upper right');  
 if not single:
 	filename = 'all'
-plt.savefig(prefix+'WienerDistribution/'+str(filename)+'_wiener.png')
+plt.savefig(prefix+'WienerDistribution/'+str(filename)+'_polarity.png')
 plt.cla()
