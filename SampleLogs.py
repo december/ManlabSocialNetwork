6ca6f18b10df23166a9ff9a4ac4cd16936cf0441
december <luyunfeicx@163.com>
2018-01-31 23:34:44 +0800

    fix bug

--- SampleLogs.py
+++ SampleLogs.py
@@ -8,38 +8,53 @@ data = fr.readlines()
 fr.close()
 
 path = '../../cascading_generation_model/all/networks/'
-prefix = '../../cascading_generation_model/all/behavior/'
+prefix = '../../cascading_generation_model/all/behavior_origin/'
 namelist = os.listdir(path)
 pointdic = {}
 behaviordic = {}
+tweetdic = {}
+maxsize = {}
+totalnum = {}
 for name in namelist:
 	fr = open(path+name, 'r')
 	points = fr.readlines()
 	fr.close()
 	pointdic[name] = {}
+	maxsize[name] = 0
+	totalnum[name] = 0
 	behaviordic[name] = list()
 	for p in points:
 		pointdic[name][p[:-1]] = 1
 print 'Begin collecting...'
 
-def CollectLog(info, k, m):
+def CollectLog(info, k, m, td):
 	for i in range(m):
 		temp = info[i][:-1].split('\t')
-		if pointdic[k].has_key(temp[1]) and (temp[4] == '-1' or pointdic[k].has_key(temp[4])):
-			behaviordic[k].append(info[i])
+		if pointdic[k].has_key(temp[1]) and (temp[4] == '-1' or pointdic[k].has_key(temp[4])) and (temp[3] == '-1' or tweetdic.has_key(temp[3])):
+			td.append(info[i])
+			tweetdic[temp[3]] = 1
+	return td
 
 n = len(data)
 print n
 i = 0
 cascade = 0
+
 while i < n:
 	temp = data[i].split('\t')
 	number = int(temp[1]) + 1
 	author = data[i+1].split('\t')[1]
 	for key in pointdic:
 		if pointdic[key].has_key(author):
-			behaviordic[key].append(data[i])
-			CollectLog(data[i+1:i+number], key, number - 1)
+			tempdata = list()
+			tweetdic = {}
+			tempdata.append(temp[0])
+			tempdata = CollectLog(data[i+1:i+number], key, number - 1, tempdata)
+			length = len(tempdata) - 1
+			tempdata[0] = tempdata[0] + '\t' + str(length) + '\n'
+			maxsize[key] = max(maxsize[key], length)
+			totalnum[key] += length
+			behaviordic[key].extend(tempdata)
 	cascade += 1
 	if cascade % 1000000 == 0:
 		print str(cascade) + '(' + str(i) + ')'
@@ -47,7 +62,7 @@ while i < n:
 print 'Begin output...'
 
 for name in namelist:
-	fw = open(prefix+name, 'w')
+	fw = open(prefix+name+'_'+str(maxsize[name])+'_'+str(totalnum[name]), 'w')
 	for line in behaviordic[name]:
 		fw.write(line)
 	fw.close()
