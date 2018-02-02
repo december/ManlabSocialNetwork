689e55528cb2f1aac42524d5e8248adcb418fb0a
december <luyunfeicx@163.com>
2018-02-02 21:24:11 +0800

    fix bug

--- GeneratorPoisson_2pi.py
+++ GeneratorPoisson_2pi.py
@@ -26,15 +26,15 @@ uid = list() #from user index in this ego network to user id
 iddic = {} #from user id to user index in this ego network
 tweetdic = {} #from tweet id to the user index of its author
 number = 0 #total number of tweeters
-k = 1.025
+k = 1.03
 normal_rand = scipy.stats.truncnorm.rvs(0, 1, loc=0, scale=1, size=100000000)
 nrpos = 0
 
 def GetIET(l):
 	global nrpos
-	#p = numpy.random.rand()
-	p = normal_rand[nrpos]
-	nrpos += 1
+	p = numpy.random.rand()
+	#p = normal_rand[nrpos]
+	#nrpos += 1
 	t = -1 * np.log(1-p) / l
 	return round(t)
 
@@ -78,8 +78,8 @@ def GetLog(r, p, u, t, tau, c, d): #root_tweet, parent_tweet, parent_user, paren
 		#thres = x[edgemap[u][f]] ** -(d - 1) * pi[edgemap[u][f]] * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)
 		realpi = pi[edgemap[u][f]]
 		if d > 1:
-			realpi = x[edgemap[u][f]]
-		thres = realpi * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f) * k ** -(d - 1)
+			realpi = x[edgemap[u][f]] * k ** -(d - 1)
+		thres = realpi * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)
 		prob = normal_rand[nrpos]
 		nrpos += 1
 		if prob <= thres:
