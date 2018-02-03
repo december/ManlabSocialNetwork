ab566bdadbbabdc8932ea19d5b80bf4aa27eb55e
december <luyunfeicx@163.com>
2018-02-03 17:41:47 +0800

    change sp

--- EM_tensorflow_sp.py
+++ EM_tensorflow_sp.py
@@ -65,31 +65,43 @@ epsilon = 10.0 #when will EM stop
 lbd = np.zeros(users) #parameter lambda which have calculated before
 count = 0
 
-def Joint(omega, pi, x, theta1, theta2, theta3, theta4):
-	param = np.append(omega, pi)
+def Joint(omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post):
+	param = np.append(omega, pi0)
+	param = np.append(param, pi1)
 	param = np.append(param, x)
 	param = np.append(param, theta1)
 	param = np.append(param, theta2)
 	param = np.append(param, theta3)
 	param = np.append(param, theta4)
+	param = np.append(param, theta1_post)
+	param = np.append(param, theta2_post)
+	param = np.append(param, theta3_post)
+	param = np.append(param, theta4_post)
 	return param
 
 def Resolver(param):
 	omega = param[:poslist[0]]
-	pi = param[poslist[0]:poslist[1]]
-	x = param[poslist[1]:poslist[2]]
-	theta1 = param[poslist[2]:poslist[3]]
-	theta2 = param[poslist[3]:poslist[4]]
-	theta3 = param[poslist[4]:poslist[5]]
-	theta4 = param[poslist[5]:]
-	return omega, pi, x, theta1, theta2, theta3, theta4
+	pi0 = param[poslist[0]:poslist[1]]
+	pi1 = param[poslist[1]:poslist[2]]
+	x = param[poslist[2]:poslist[3]]
+	theta1 = param[poslist[3]:poslist[4]]
+	theta2 = param[poslist[4]:poslist[5]]
+	theta3 = param[poslist[5]:poslist[6]]
+	theta4 = param[poslist[6]:poslist[7]]
+	theta1_post = param[poslist[7]:poslist[8]]
+	theta2_post = param[poslist[8]:poslist[9]]
+	theta3_post = param[poslist[9]:poslist[10]]
+	theta4_post = param[poslist[10]:]
+	return omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post
 
-def Select(omega, pi, x, theta1, theta2, theta3, theta4):
+def Select(omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post):
 	p = list()
 	for i in range(vnum):
 		p.append(omega[vlist[i]])
 	for i in range(enum):
-		p.append(pi[elist[i]])
+		p.append(pi0[elist[i]])
+	for i in range(enum):
+		p.append(pi1[elist[i]])
 	p.append(x[0])
 	for i in range(vnum):
 		p.append(theta1[vlist[i]])	
@@ -99,6 +111,14 @@ def Select(omega, pi, x, theta1, theta2, theta3, theta4):
 		p.append(theta3[vlist[i]])
 	for i in range(vnum):
 		p.append(theta4[vlist[i]])
+	for i in range(vnum):
+		p.append(theta1_post[vlist[i]])	
+	for i in range(vnum):
+		p.append(theta2_post[vlist[i]])
+	for i in range(vnum):
+		p.append(theta3_post[vlist[i]])
+	for i in range(vnum):
+		p.append(theta4_post[vlist[i]])
 	return Resolver(np.array(p))
 
 def Phi(theta1, theta2, theta3, theta4, idx):
@@ -123,10 +143,10 @@ def Phi_np(theta1, theta2, theta3, theta4, idx):
 		return np.sin(theta1) * np.sin(theta1) * np.sin(theta2) * np.sin(theta2) * np.sin(theta3) * np.sin(theta3) * np.cos(theta4) * np.cos(theta4)
 	return np.sin(theta1) * np.sin(theta1) * np.sin(theta2) * np.sin(theta2) * np.sin(theta3) * np.sin(theta3) * np.sin(theta4) * np.sin(theta4)
 
-def LnLc(omega, pi, x, philist, c): #ln fromulation of one cascades's likelihood on tau(do not include part of Q)
+def LnLc(omega, pi0, pi1, x, philist, philist_post, c): #ln fromulation of one cascades's likelihood on tau(do not include part of Q)
 	uc = cascade_author[c]
 	tmplbd = tf.log(lbd[vlist_tf[uc]])
-	tmpphi = philist[uc]
+	tmpphi = philist_post[uc]
 	s = tf.cast(tf.log(tmpphi) + tmplbd, dtype=tf.float64)
 	#print tf.shape(s)
 
@@ -140,7 +160,10 @@ def LnLc(omega, pi, x, philist, c): #ln fromulation of one cascades's likelihood
 	nc_id = tf.gather(nrusc_id, nrusc_dic[bn:en], axis=0)
 
 	omega_rc = tf.gather(omega, rc_id[:, 1], axis=0)
-	pi_rc = tf.gather(pi, rc_id[:, 0], axis=0)
+	pi0_rc = tf.gather(pi0, rc_id[:, 0], axis=0)
+	pi1_rc = tf.gather(pi1, rc_id[:, 0], axis=0)
+	sign_rc = tf.cast(tf.less(rc[:, 1], 1.5), dtype=tf.float64)
+	pi_rc = pi1_rc * sign_rc + pi0_rc * (1 - sign_rc)
 	#x_rc = tf.gather(x, rc_id[:, 0], axis=0)
 	phi_rc = tf.gather(philist, rc_id[:, 1], axis=0)
 	oldtmp = tf.reduce_sum(tf.log(omega_rc) - omega_rc * rc[:, 0] + tf.log(pi_rc) - rc[:, 1] * tf.log(x))
@@ -150,7 +173,10 @@ def LnLc(omega, pi, x, philist, c): #ln fromulation of one cascades's likelihood
 	s += tf.reduce_sum(tf.log(phi_rc), 0)	
 
 	omega_nc = tf.gather(omega, nc_id[:, 1], axis=0)
-	pi_nc = tf.gather(pi, nc_id[:, 0], axis=0)
+	pi0_nc = tf.gather(pi0, nc_id[:, 0], axis=0)
+	pi1_nc = tf.gather(pi1, nc_id[:, 0], axis=0)
+	sign_nc = tf.cast(tf.less(nc[:, 1], 1.5), dtype=tf.float64)
+	pi_nc = pi1_nc * sign_nc + pi0_nc * (1 - sign_nc)
 	#x_nc = tf.gather(x, nc_id[:, 0], axis=0)
 	exponent = tf.maximum(-1 * omega_nc * nc[:, 0], -100)
 	estimate = tf.exp(exponent) - 1
@@ -172,8 +198,8 @@ def QMatrix():
 	qmx = tf.stack(qmx, 0)
 	return tf.reshape(qmx, shape=(n, 5))
 
-def QF(omega, pi, x, philist, c): #calculate q funciton with tricks
-	lc = LnLc(omega, pi, x, philist, c)
+def QF(omega, pi0, pi1, x, philist, philist_post, c): #calculate q funciton with tricks
+	lc = LnLc(omega, pi0, pi1, x, philist, philist_post, c)
 	s = list()
 	for i in range(5):
 		temps = 0
@@ -185,28 +211,29 @@ def QF(omega, pi, x, philist, c): #calculate q funciton with tricks
 def printInfo(obj, i, noreply):
 	print str(i) + ' ' + str(obj) + ' ' + str(noreply)
 
-def cond(obj, i, noreply, omega, pi, x, philist, qm):
+def cond(obj, i, noreply, omega, pi0, pi1, x, philist, philist_post, qm):
 	return i < q.get_shape()[0]
 
-def body(obj, i, noreply, omega, pi, x, philist, qm):
+def body(obj, i, noreply, omega, pi0, pi1, x, philist, philist_post, qm):
 	#if rusc_dic[i].get_shape()[0] == 0:
 	if begin_rusc[i] == end_rusc[i]:
 		if noreply[cascade_author[i]] == 0:
 			noreply[cascade_author[i]] += tf.reduce_sum(qm[i] * tf.log(qm[i]))
-			noreply[cascade_author[i]] -= tf.reduce_sum(qm[i] * LnLc(omega, pi, x, philist, i))
+			noreply[cascade_author[i]] -= tf.reduce_sum(qm[i] * LnLc(omega, pi0, pi1, x, philist, philist_post, i))
 		obj += noreply[cascade_author[i]]
 	else:
 		obj += tf.reduce_sum(qm[i] * tf.log(qm[i]))
-		obj -= tf.reduce_sum(qm[i] * LnLc(omega, pi, x, philist, i))
+		obj -= tf.reduce_sum(qm[i] * LnLc(omega, pi0, pi1, x, philist, philist_post, i))
 	i += 1
 	#tf.py_func(printInfo, [obj, i, noreply], tf.float64)
-	return obj, i, noreply, omega, pi, x, philist, qm
+	return obj, i, noreply, omega, pi0, pi1, x, philist, philist_post, qm
 
 def ObjF(param, qm): #formulation of objective function (include barrier) (the smaller the better)
-	omega, pi, x, theta1, theta2, theta3, theta4 = Resolver(param)
+	omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post = Resolver(param)
 	omega = tf.cos(omega) * tf.cos(omega)
 	#pi = tf.cos(pi) * tf.cos(pi)
-	pi = pi * pi
+	pi0 = pi0 * pi0
+	pi1 = pi1 * pi1
 	#x = x * x
 	philist = list()
 	for i in range(5):
@@ -214,6 +241,13 @@ def ObjF(param, qm): #formulation of objective function (include barrier) (the s
 	philist = tf.stack(philist)
 	philist = tf.reshape(philist, (5, -1))
 	philist = tf.transpose(philist)
+
+	philist_post = list()
+	for i in range(5):
+		philist.append(Phi(theta1_post, theta2_post, theta3_post, theta4_post, i))
+	philist_post = tf.stack(philist_post)
+	philist_post = tf.reshape(philist_post, (5, -1))
+	philist_post = tf.transpose(philist_post)
 	#global total
 	#total += 1
 	it = tf.cast(0, tf.int32)
@@ -229,28 +263,29 @@ def ObjF(param, qm): #formulation of objective function (include barrier) (the s
 	'''
 	#obj = (tf.reduce_sum(tf.log(omega)) + tf.reduce_sum(tf.log(x)) + tf.reduce_sum(tf.log(1-pi)) + tf.reduce_sum(tf.log(pi))) * gamma #need to be fixxed
 	obj = tf.cast(0.0, tf.float64)
-	newobj, _, _, _, _, _, _, _ = tf.while_loop(cond, body, [obj, it, noreply, omega, pi, x, philist, qm], parallel_iterations=80)
+	newobj, _, _, _, _, _, _, _, _, _ = tf.while_loop(cond, body, [obj, it, noreply, omega, pi0, pi1, x, philist, philist_post, qm], parallel_iterations=80)
 		
 	#if total % 10000 == 0:
 	#	print 'No.' + str(total) + ' times: ' + str(obj)
 	return newobj
 
-def cond_e(i, omega, pi, x, philist):
+def cond_e(i, omega, pi0, pi1, x, philist, philist_post):
 	return i < q.get_shape()[0]
 
-def body_e(i, omega, pi, x, philist):
-	s = QF(omega, pi, x, philist, i)
+def body_e(i, omega, pi0, pi1, x, philist, philist_post):
+	s = QF(omega, pi0, pi1, x, philist, philist_post, i)
 	tf.assign(q[i], s)
 	#q = q[i].assign(s)
 	#q.append(s)
 	i += 1
-	return i, omega, pi, x, philist
+	return i, omega, pi0, pi1, x, philist, philist_post
 
-def EStep(omega, pi, x, theta1, theta2, theta3, theta4): #renew q and lc
+def EStep(omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post): #renew q and lc
 	#print [len(omega), len(pi), len(x)]
 	omega = tf.cos(omega) * tf.cos(omega)
 	#pi = tf.cos(pi) * tf.cos(pi)
-	pi = pi * pi
+	pi0 = pi0 * pi0
+	pi1 = pi1 * pi1
 	#x = x * x
 	#print [len(oc), len(pc), len(xc)]
 	philist = list()
@@ -259,10 +294,17 @@ def EStep(omega, pi, x, theta1, theta2, theta3, theta4): #renew q and lc
 	philist = tf.stack(philist)
 	philist = tf.reshape(philist, (5, -1))
 	philist = tf.transpose(philist)
+
+	philist_post = list()
+	for i in range(5):
+		philist.append(Phi(theta1_post, theta2_post, theta3_post, theta4_post, i))
+	philist_post = tf.stack(philist_post)
+	philist_post = tf.reshape(philist_post, (5, -1))
+	philist_post = tf.transpose(philist_post)
 	#count = 0
 	it = 0
 	#q = list()
-	newit, _, _, _, _ = tf.while_loop(cond_e, body_e, [it, omega, pi, x, philist], parallel_iterations=80)
+	newit, _, _, _, _, _, _ = tf.while_loop(cond_e, body_e, [it, omega, pi0, pi1, x, philist, philist_post], parallel_iterations=80)
 	#for c in q:
 		#QF(omega, pi, x, philist, c)
 		#count += 1
@@ -377,11 +419,6 @@ theta2 = np.zeros(allusers) #one of spherical coordinates of phi distribution
 theta3 = np.zeros(allusers) #one of spherical coordinates of phi distribution
 theta4 = np.zeros(allusers) #one of spherical coordinates of phi distribution
 
-theta1_post = np.zeros(allusers) #one of spherical coordinates of phi distribution
-theta2_post = np.zeros(allusers) #one of spherical coordinates of phi distribution
-theta3_post = np.zeros(allusers) #one of spherical coordinates of phi distribution
-theta4_post = np.zeros(allusers) #one of spherical coordinates of phi distribution
-
 if filename == 0:
 	fr = open(prefix+'lda'+suffix, 'r')
 	ldainfo = fr.readlines()
@@ -427,6 +464,11 @@ if filename == 3:
 		theta2[i] = np.arccos(np.sqrt(newtr[1] / (1 - newtr[0])))
 		theta3[i] = np.arccos(np.sqrt(newtr[2] / (1 - newtr[0] - newtr[1])))
 		theta4[i] = np.arccos(np.sqrt(newtr[3] / (newtr[3] + newtr[4])))
+
+theta1_post = theta1.copy() #one of spherical coordinates of phi distribution
+theta2_post = theta2.copy() #one of spherical coordinates of phi distribution
+theta3_post = theta3.copy() #one of spherical coordinates of phi distribution
+theta4_post = theta4.copy() #one of spherical coordinates of phi distribution
 
 phi_initial = list()
 for i in range(5):
@@ -480,10 +522,49 @@ pi = np.array(pi)
 #pi = np.arccos(np.sqrt(pi))
 pi = np.sqrt(pi)
 x = np.array([1.072])
+pi0 = pi.copy()
+pi1 = pi.copy()
 
 print sum(lbd) * 100 / users
 omega += sum(lbd) * 50000 / users
 omega = np.arccos(np.sqrt(omega))
+
+if filename < 0:
+	fr = open(prefix+'phi_post_initial'+suffix, 'r')
+	ldainfo = fr.readlines()
+	print 'Use former parameter as initial distribution...'
+	for i in range(allusers):
+		temp = ldainfo[i].split('\t')
+		idx = iddic[temp[0]]
+		theta1_post[idx] = np.arccos(np.sqrt(float(temp[1])))
+		theta2_post[idx] = np.arccos(np.sqrt(float(temp[2]) / (1 - float(temp[1]))))
+		theta3_post[idx] = np.arccos(np.sqrt(float(temp[3]) / (1 - float(temp[1]) - float(temp[2]))))
+		theta4_post[idx] = np.arccos(np.sqrt(float(temp[4]) / (float(temp[4]) + float(temp[5]))))
+	fr.close()
+
+	fr = open(prefix+'phi_initial'+suffix, 'r')
+	ldainfo = fr.readlines()
+	for i in range(allusers):
+		temp = ldainfo[i].split('\t')
+		idx = iddic[temp[0]]
+		theta1[idx] = np.arccos(np.sqrt(float(temp[1])))
+		theta2[idx] = np.arccos(np.sqrt(float(temp[2]) / (1 - float(temp[1]))))
+		theta3[idx] = np.arccos(np.sqrt(float(temp[3]) / (1 - float(temp[1]) - float(temp[2]))))
+		theta4[idx] = np.arccos(np.sqrt(float(temp[4]) / (float(temp[4]) + float(temp[5]))))
+	fr.close()
+
+	fr = open(prefix+'pi_initial'+suffix, 'r')
+	ldainfo = fr.readlines()
+	for i in range(allusers):
+		temp = ldainfo[i].split('\t')
+		idx = edgemap[iddic[temp[0]]][iddic[temp[1]]]
+		if temp[2] == '0':
+			pi0[idx] = float(temp[3])
+		else:
+			pi1[idx] = float(temp[3])
+	fr.close()
+	pi0 = np.sqrt(pi0)
+	pi1 = np.sqrt(pi1)
 
 #Read personal cascade file
 print 'Read behavior log...'
@@ -497,11 +578,12 @@ for i in range(users):
 
 poslist.append(vnum)
 poslist.append(vnum+enum)
-poslist.append(vnum+enum+1)
-for i in range(4):
-	poslist.append(vnum*(i+2)+enum+1)
-omega, pi, x, theta1, theta2, theta3, theta4 = Select(omega, pi, x, theta1, theta2, theta3, theta4)
-print 'There are ' + str(vnum * 5) + ' point parameters and ' + str(enum + 1) + ' edge parameters to be learned...'
+poslist.append(vnum+2*enum)
+poslist.append(vnum+2*enum+1)
+for i in range(8):
+	poslist.append(vnum*(i+2)+2*enum+1)
+omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post = Select(omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post)
+print 'There are ' + str(vnum * 9) + ' point parameters and ' + str(2 * enum + 1) + ' edge parameters to be learned...'
 #Conduct EM algorithm
 #QMatrix(q)
 for c in clist:
@@ -574,7 +656,7 @@ if single:
 	prefix = prefix + 'single_user_parameter/'
 	suffix = '_' + str(filename) + suffix
 
-def Output(omega, pi, x, theta1, theta2, theta3, theta4):
+def Output(omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post):
 	print 'Output data files...'
 	fw = open(prefix+'omega_Poisson'+suffix, 'w')
 	for i in range(vnum):
@@ -593,12 +675,12 @@ def Output(omega, pi, x, theta1, theta2, theta3, theta4):
 			fw.write('\t')
 			fw.write(uid[fd])
 			fw.write('\t')
-			fw.write(str(pi[edic[edgemap[item][fd]]]))
+			fw.write(str(pi0[edic[edgemap[item][fd]]]))
 			fw.write('\n')
 	fw.close()
 
 	print x
-	x = np.zeros(len(pi)) + x
+
 	fw = open(prefix+'x_Poisson'+suffix, 'w')
 	for item in edgemap:
 		for fd in edgemap[item]:
@@ -608,7 +690,7 @@ def Output(omega, pi, x, theta1, theta2, theta3, theta4):
 			fw.write('\t')
 			fw.write(uid[fd])
 			fw.write('\t')
-			fw.write(str(x[edic[edgemap[item][fd]]]))
+			fw.write(str(pi1[edic[edgemap[item][fd]]]))
 			fw.write('\n')
 	fw.close()
 
@@ -622,11 +704,21 @@ def Output(omega, pi, x, theta1, theta2, theta3, theta4):
 			fw.write('\n')
 		fw.close()
 
+	for i in range(5):
+		fw = open(prefix+'phi'+str(i)+'_Poisson_post'+suffix, 'w')
+		phi = Phi_np(theta1_post, theta2_post, theta3_post, theta4_post, i)
+		for j in range(vnum):
+			fw.write(uid[vlist[j]])
+			fw.write('\t')
+			fw.write(str(phi[j]))
+			fw.write('\n')
+		fw.close()
+
 changed1 = False
 changed2 = False
 with tf.Session() as session:
 	session.run(init)
-	qf = EStep(omega, pi, x, theta1, theta2, theta3, theta4)
+	qf = EStep(omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post)
 	print 'EStep part construction finished.'
 	out_qf = session.run(qf)
 	obj = session.run(target, feed_dict={qm:out_qf})
@@ -658,14 +750,14 @@ with tf.Session() as session:
 		#print omega[:10]
 		if abs(lastObj) - obj < epsilon:
 			if abs(lastObj) - obj > 0:
-				omega, pi, x, theta1, theta2, theta3, theta4 = Resolver(newp)
+				omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post = Resolver(newp)
 				break
 			else:
 				alpha = alpha / 2
-		omega, pi, x, theta1, theta2, theta3, theta4 = Resolver(newp)
+		omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post = Resolver(newp)
 		#Output(omega, pi, x, theta1, theta2, theta3, theta4)
 		#Output(np.cos(omega) * np.cos(omega), np.cos(pi) * np.cos(pi), x, theta1, theta2, theta3, theta4)
-		Output(np.cos(omega) * np.cos(omega), pi * pi, x, theta1, theta2, theta3, theta4)
+		Output(np.cos(omega) * np.cos(omega), pi0 * pi0, pi1 * pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post)
 		lastObj = obj
 		#if not changed1 and obj <= 20000000:
 		#	alpha = alpha / 2
@@ -681,7 +773,7 @@ pi = pi * pi
 x = x
 
 #Output parameters
-Output(omega, pi, x, theta1, theta2, theta3, theta4)
+Output(omega, pi0, pi1, x, theta1, theta2, theta3, theta4, theta1_post, theta2_post, theta3_post, theta4_post)
 
 endtime = datetime.datetime.now()
 print 'Time consumed: ' + str(endtime - starttime) + ' (' + str(a) + ')'
