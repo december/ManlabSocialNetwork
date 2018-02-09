b308a548428a7569f0e8a8dbed247f73d9e16bac
december <luyunfeicx@163.com>
2018-02-10 00:38:00 +0800

    try 0.33

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -251,8 +251,8 @@ for i in range(n):
 		d = tau + 1
 		if tau > 0:
 			d = 1
-		for ui in range(1, 5):
-			s += (GetExpect(poineer[tau], ui, d, 1, 0) + 1) * 0.25
+		for ui in range(2, 5):
+			s += (GetExpect(poineer[tau], ui, d, 1, 0) + 1) / 3
 		'''
 		if not expect_pop.has_key(poineer[tau]):
 			expect_pop[poineer[tau]] = list()
