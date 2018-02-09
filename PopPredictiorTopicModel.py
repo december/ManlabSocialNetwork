4e87a0b17323e4bc0c87b3f710a3e32135288a28
december <luyunfeicx@163.com>
2018-02-10 01:37:27 +0800

    cut 1

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -243,9 +243,9 @@ for i in range(n):
 		for tau in range(1, 10):
 			mul *=  GetPhi(phi1, phi2, phi3, phi4, phi5, j, poineer[tau])
 		infer.append(mul)
-	norm = infer[2] + infer[3] + infer[1]
-	for j in range(3):
-		infer[j+2] = infer[j+2] / norm
+	norm = infer[2] + infer[3] + infer[1] + infer[4]
+	for j in range(4):
+		infer[j+1] = infer[j+1] / norm
 	s = 0
 	temps = 0
 	num = 0
@@ -253,7 +253,7 @@ for i in range(n):
 		d = tau + 1
 		if tau > 0:
 			d = 1
-		for ui in range(1, 4):
+		for ui in range(1, 5):
 			s += GetExpect(poineer[tau], ui, d, 1, 0) * infer[ui]
 			
 		#s += GetExpect(poineer[tau], 4, d, 1, 0)
