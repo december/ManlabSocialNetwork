78e59973fe2675393b8f94c0a037e5ac2d881551
路云飞 <luyunfeicx@163.com>
2018-02-09 12:52:12 +0800

    find bug

--- SampleLogs.py
+++ SampleLogs.py
@@ -37,14 +37,15 @@ def CollectLog(info, k, m, td):
 
 n = len(data)
 print n
-#i = 1589440404
-i = 0
+i = 2234519257
+#i = 0
 cascade = 0
 wrongdata = 0
 
 while i < n:
 	if i == 1590678642:
 		i = 1590678668
+	print i
 	temp = data[i].split('\t')
 	#if len(temp[1]) > 10 and i < 1590678665:
 	#	i = 1590678668
