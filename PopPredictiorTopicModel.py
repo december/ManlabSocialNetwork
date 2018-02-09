fb0cb01e3f554ca08439989b4fab1973ce4c9dcf
december <luyunfeicx@163.com>
2018-02-10 01:08:10 +0800

    remove cuts

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -229,9 +229,9 @@ for i in range(n):
 	flag = False
 	poineer = list()
 	for j in line:
-		if j == '1' or j == '206430' or j == '178365' or j == '178826':
-			flag = True
-			break
+		#if j == '1' or j == '206430' or j == '178365' or j == '178826':
+		#	flag = True
+		#	break
 		poineer.append(iddic[int(j)])
 	if flag:
 		wrong += 1
