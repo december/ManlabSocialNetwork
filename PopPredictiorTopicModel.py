9f9e257341da29dba42aa595df9b798e6e47b523
december <luyunfeicx@163.com>
2018-02-10 01:37:59 +0800

    cut 1

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -254,7 +254,7 @@ for i in range(n):
 		if tau > 0:
 			d = 1
 		for ui in range(1, 5):
-			s += GetExpect(poineer[tau], ui, d, 1, 0) * infer[ui]
+			s += GetExpect(poineer[tau], ui, d, 1, 0) * 0.25
 			
 		#s += GetExpect(poineer[tau], 4, d, 1, 0)
 	s += 10
