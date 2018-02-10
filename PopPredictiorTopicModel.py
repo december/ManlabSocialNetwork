e8cbd5c0d96a91342706859d8c97d4b9dc8e377f
december <luyunfeicx@163.com>
2018-02-10 18:29:59 +0800

    try 0

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -80,14 +80,14 @@ def GetExpect(u, tau, d, rp, s): #root_tweet, parent_tweet, parent_user, parent_
 
 def GetRanking(u, taudb, cset, uid):
 	result = {}
-	print uid
+	#print uid
 	for f in edgemap[u]:
 		if not f in cset:
 			continue
 		p = 0
 		iterset = [1,3]
 		if uid == '1':
-			iterset = [0,2]
+			iterset = [0,1,2,3,4]
 		for tau in iterset:
 			#p += pi[edgemap[u][f]] * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f) * taudb[tau]
 			#p = np.log(GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)) + np.log(taudb[tau]) + np.log(x[edgemap[u][f]])
