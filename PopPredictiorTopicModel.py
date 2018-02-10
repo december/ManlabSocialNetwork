f9fab583e894c745cc0ada2ae0edad6efd7cb495
december <luyunfeicx@163.com>
2018-02-10 11:51:31 +0800

    fix bug

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -247,7 +247,7 @@ for i in range(n):
 	infer = list()
 	for j in range(5):
 		#mul = np.log(expect_pop[poineer[0]][j])
-		#mul = 0
+		mul = 0
 		for tau in range(1, 5):
 			mul +=  np.log(GetPhi(phi1, phi2, phi3, phi4, phi5, j, poineer[tau]))
 		infer.append(mul)
