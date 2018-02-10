3fffb167fc8351070cbd8c346d2bc73c87f3cdf5
december <luyunfeicx@163.com>
2018-02-10 18:36:35 +0800

    try separate

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -93,8 +93,8 @@ def GetRanking(u, taudb, cset, uid):
 			#p = np.log(GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)) + np.log(taudb[tau]) + np.log(x[edgemap[u][f]])
 			#p = np.log(taudb[tau]) + np.log(x[edgemap[u][f]])
 			p = np.log(x[edgemap[u][f]]) + np.log(taudb[tau])
-			#if uid == '1':
-			#	p += GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)
+			if uid == '1':
+				p = np.log(pi[edgemap[u][f]]) + np.log(taudb[tau])
 		result[f] = p
 	return sorted(result.iteritems(), key=lambda d:d[1], reverse=True)
 
