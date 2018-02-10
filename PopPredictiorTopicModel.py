3d7dfaa1e72c0758f8f2eda7c2f6858e0a5d748f
december <luyunfeicx@163.com>
2018-02-10 17:35:14 +0800

    try notopic

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -88,8 +88,8 @@ def GetRanking(u, taudb, cset):
 		for tau in iterset:
 			#p += pi[edgemap[u][f]] * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f) * taudb[tau]
 			#p = np.log(GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f)) + np.log(taudb[tau]) + np.log(x[edgemap[u][f]])
-			#p = np.log(taudb[tau]) + np.log(x[edgemap[u][f]])
-			p = np.log(x[edgemap[u][f]])
+			p = np.log(taudb[tau]) * np.log(x[edgemap[u][f]])
+			#p = np.log(x[edgemap[u][f]])
 		result[f] = p
 	return sorted(result.iteritems(), key=lambda d:d[1], reverse=True)
 
