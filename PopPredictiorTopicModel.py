05dff49bc70c63f1eedd59d61a94ba9fd8cf2915
december <luyunfeicx@163.com>
2018-02-10 12:35:50 +0800

    try 2

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -246,8 +246,8 @@ for i in range(n):
 	delta = 0
 	infer = list()
 	for j in range(5):
-		#mul = np.log(expect_pop[poineer[0]][j])
-		mul = 0
+		mul = np.log(expect_pop[poineer[0]][j])
+		#mul = 0
 		for tau in range(0, 5):
 			mul +=  np.log(GetPhi(phi1, phi2, phi3, phi4, phi5, j, poineer[tau]))
 		infer.append(mul)
