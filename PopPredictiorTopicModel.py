8ac4023c76c8a12d3cd8c05237520713ceaf943c
december <luyunfeicx@163.com>
2018-02-10 15:40:12 +0800

    try participation

--- PopPredictiorTopicModel.py
+++ PopPredictiorTopicModel.py
@@ -83,7 +83,7 @@ def GetRanking(u, taudb):
 		p = 0
 		for tau in range(5):
 			#p += pi[edgemap[u][f]] * GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f) * taudb[tau]
-			p = GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f) * taudb[tau] * pi[edgemap[u][f]]
+			p = GetPhi(phi1, phi2, phi3, phi4, phi5, tau, f) * taudb[tau] * x[edgemap[u][f]]
 		result[f] = p
 	return sorted(result.iteritems(), key=lambda d:d[1], reverse=True)
 
