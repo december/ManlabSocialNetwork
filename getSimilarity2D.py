3ec42b932785b4401ee4fb3e2b2ddfdbbaef1257
december <luyunfeicx@163.com>
2018-02-11 01:17:00 +0800

    mape

--- getSimilarity2D.py
+++ getSimilarity2D.py
@@ -33,7 +33,7 @@ j_matrix_sim = np.zeros((m, m))
 fr1 = open(prefix+'similarity/'+filename+'_pearson_value.detail', 'r')
 data = fr1.readlines()
 n = len(data)
-n = 20
+#n = 20
 for i in range(n):
 	temp = data[i].split('\t')[:-1]
 	for j in range(n):
@@ -44,7 +44,7 @@ fr1.close()
 fr2 = open(prefix+'similarity/'+filename+'_jaccard_value.detail', 'r')
 data = fr2.readlines()
 n = len(data)
-n = 20
+#n = 20
 for i in range(n):
 	temp = data[i].split('\t')[:-1]
 	for j in range(n):
@@ -56,7 +56,7 @@ filename = sys.argv[1]
 fr1 = open(prefix+'similarity/'+filename+'_pearson_value.detail', 'r')
 data = fr1.readlines()
 n = len(data)
-n = 20
+#n = 20
 for i in range(n):
 	temp = data[i].split('\t')[:-1]
 	for j in range(n):
@@ -67,7 +67,7 @@ fr1.close()
 fr2 = open(prefix+'similarity/'+filename+'_jaccard_value.detail', 'r')
 data = fr2.readlines()
 n = len(data)
-n = 20
+#n = 20
 for i in range(n):
 	temp = data[i].split('\t')[:-1]
 	for j in range(n):
@@ -77,19 +77,25 @@ fr2.close()
 
 delta_p = np.zeros((n, n))
 delta_j = np.zeros((n, n))
+psize = 0
+jsize = 1
 for i in range(n):
 	for j in range(n):
 		if p_matrix_real[i][j] != 0:
-			delta_p[i][j] = abs(p_matrix_real[i][j] - p_matrix_sim[i][j]) / abs(p_matrix_real[i][j])
+			delta_p[i][j] = abs(p_matrix_real[i][j] - p_matrix_sim[i][j])
+			psize += 1
 		if j_matrix_real[i][j] != 0:
-			delta_j[i][j] = abs(j_matrix_real[i][j] - j_matrix_sim[i][j]) / abs(j_matrix_real[i][j])
-print np.sum(delta_p) * 1.0 / delta_p.size 
-print np.sum(delta_j) * 1.0 / delta_j.size
+			delta_j[i][j] = abs(j_matrix_real[i][j] - j_matrix_sim[i][j])
+			jsize += 1
+print np.sum(delta_p) * 1.0 / psize 
+print np.sum(delta_j) * 1.0 / jsize
+'''
 sns.set()
 ax = sns.heatmap(delta_p, norm=LogNorm(vmin=delta_p.min(), vmax=delta_p.max()))
 #ax = sns.heatmap(delta_p[:1000][:1000])
 plt.savefig(prefix+'similarity/'+filename+'_pearson_2D.png')
 plt.cla()
+'''
 #df = pd.DataFrame(j_matrix)
 #sns.heatmap(df, vmin=np.min(p_matrix), vmax=np.max(p_matrix))
 #plt.savefig(prefix+'similarity/'+filename+'_jaccard_2D.png')
