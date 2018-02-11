4712c5b56220fdb4aa29a5bb61731c018b3d932a
december <luyunfeicx@163.com>
2018-02-11 22:13:49 +0800

    try annotation

--- getPointSimilarityDistribution.py
+++ getPointSimilarityDistribution.py
@@ -155,7 +155,7 @@ plt.plot(px, ps, '#4876ff', label='Our Framework', linewidth=2.5)
 plt.plot(px, ps1, '#8c8c8c',linestyle='--',label='BP', linewidth=2.5)
 plt.plot(px, ps2, '#458b00',linestyle='--',label='Base', linewidth=2.5)
 plt.plot([0.2, 0.2,], [0, py[9]], 'k:', linewidth=2.5)
-plt.ylim(0)
+plt.ylim((0,1))
 plt.xlabel(u'Collectivity', fontsize=14)
 plt.ylabel(u'CDF', fontsize=14)
 plt.legend(loc='upper right', fontsize=15);
