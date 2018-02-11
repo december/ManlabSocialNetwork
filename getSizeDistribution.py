59358cfe07a4774cd1e87df2677601a3bc8142a4
december <luyunfeicx@163.com>
2018-02-12 00:15:44 +0800

    try size

--- getSizeDistribution.py
+++ getSizeDistribution.py
@@ -68,32 +68,70 @@ while i < n:
 			real[number] = 1
 	i += number + 1
 
-namelist = os.listdir(prefix)
-position = prefix
-m = 0
-bigsim = list()
-for name in namelist:
-	if not name.endswith('.detail'):
-		continue
-	m += 1
-	fr = open(position+name, 'r')
-	simdata = fr.readlines()
-	n = len(simdata)
-	i = 0
-	while i < n:
-		temp = simdata[i].split('\t')
-		number = int(temp[1])
-		a = int(simdata[i+1].split('\t')[1])
-		if a == filename or not single:
-			if number >= 50:
-				for j in range(i, i+number+1):
-					bigsim.append(simdata[j])
-			if sim.has_key(number):
-				sim[number] += 1
-			else:
-				sim[number] = 1
-		i += number + 1
-	fr.close()
+
+fr = open('/home/luyunfei/cascading_generation_model/simulation/result/All_parameter_500.detail', 'r')
+simdata = fr.readlines()
+n = len(simdata)
+i = 0
+while i < n:
+	temp = simdata[i].split('\t')
+	number = int(temp[1])
+	a = int(simdata[i+1].split('\t')[1])
+	if sim.has_key(number):
+		sim[number] += 1
+	else:
+		sim[number] = 1
+	i += number + 1
+fr.close()
+
+sim1 = {}
+fr = open('/home/luyunfei/cascading_generation_model/simulation/result/BranchingProcess.detail', 'r')
+simdata = fr.readlines()
+n = len(simdata)
+i = 0
+while i < n:
+	temp = simdata[i].split('\t')
+	number = int(temp[1])
+	a = int(simdata[i+1].split('\t')[1])
+	if sim1.has_key(number):
+		sim1[number] += 1
+	else:
+		sim1[number] = 1
+	i += number + 1
+fr.close()
+
+sim2 = {}
+fr = open('/home/luyunfei/cascading_generation_model/simulation/result/EpidemicModel.detail', 'r')
+simdata = fr.readlines()
+n = len(simdata)
+i = 0
+while i < n:
+	temp = simdata[i].split('\t')
+	number = int(temp[1])
+	a = int(simdata[i+1].split('\t')[1])
+	if sim2.has_key(number):
+		sim2[number] += 1
+	else:
+		sim2[number] = 1
+	i += number + 1
+fr.close()
+
+sim3 = {}
+fr = open('/home/luyunfei/cascading_generation_model/simulation/result/NoTopic.detail', 'r')
+simdata = fr.readlines()
+n = len(simdata)
+i = 0
+while i < n:
+	temp = simdata[i].split('\t')
+	number = int(temp[1])
+	a = int(simdata[i+1].split('\t')[1])
+	if sim3.has_key(number):
+		sim3[number] += 1
+	else:
+		sim3[number] = 1
+	i += number + 1
+fr.close()
+
 
 realsize = sorted(real.keys())
 #print realsize
@@ -110,7 +148,7 @@ for i in range(n-1):
 simsize = sorted(sim.keys())
 #print simsize
 simnum = list()
-simcum = [sum(realcum)]
+simcum = [sum(simnum)]
 for size in simsize:
 	simnum.append(sim[size] * 1.0 / m)
 n = len(simnum)
@@ -119,59 +157,49 @@ for i in range(n-1):
 	s -= simnum[i]
 	simcum.append(s)
 
+simsize1 = sorted(sim1.keys())
+#print simsize
+simnum1 = list()
+simcum1 = [sum(simnum1)]
+for size in simsize1:
+	simnum1.append(sim1[size] * 1.0 / m)
+n = len(simnum1)
+s = sum(simnum1)
+for i in range(n-1):
+	s -= simnum1[i]
+	simcum1.append(s)
+
+simsize2 = sorted(sim2.keys())
+#print simsize
+simnum2 = list()
+simcum2 = [sum(simnum1)]
+for size in simsize1:
+	simnum2.append(sim1[size] * 1.0 / m)
+n = len(simnum2)
+s = sum(simnum2)
+for i in range(n-1):
+	s -= simnum2[i]
+	simcum2.append(s)
+
+simsize3 = sorted(sim3.keys())
+#print simsize
+simnum3 = list()
+simcum3 = [sum(simnum3)]
+for size in simsize3:
+	simnum3.append(sim3[size] * 1.0 / m)
+n = len(simnum3)
+s = sum(simnum3)
+for i in range(n-1):
+	s -= simnum3[i]
+	simcum3.append(s)
+
 realsum = sum(realnum)
 simsum = sum(simnum)
+simsum1 = sum(simnum1)
+simsum2 = sum(simnum2)
+simsum3 = sum(simnum3)
 #print simcum
 #print realcum
-
-print realsize
-newcum = list()
-newnum = list()
-num = len(realsize)
-for i in range(num):
-	newcum.append(realcum[i] * 1.0 / realsum)
-	newnum.append(realnum[i] * 1.0 / realsum)
-print newcum
-print newnum	 
-
-print simsize
-newcum = list()
-newnum = list()
-num = len(simsize)
-for i in range(num):
-	newcum.append(simcum[i] * 1.0 / simsum)
-	newnum.append(simnum[i] * 1.0 / simsum)
-print newcum
-print newnum	 
-
-
-
-logmae = [0,0]
-rs = np.array(realsize)
-rn = np.array(realcum) * 1.0 / realsum
-ss = np.array(simsize)
-sn = np.array(simcum) * 1.0 / simsum
-
-m = max(ss) + 1
-pos1 = 0
-pos2 = 0
-cr = 0
-cs = 0
-for i in range(1, m):
-	while pos1 < len(rs) and rs[pos1] < i:
-		pos1 += 1
-	while pos2 < len(ss) and ss[pos2] < i:
-		pos2 += 1
-	if pos1 < len(rs) and rs[pos1] == i:
-		cr = 1 - rn[pos1]
-	if pos2 < len(ss) and ss[pos2] == i:
-		cs = 1 - sn[pos2]
-	if pos1 >= len(rs):
-		cr = 1
-	if pos2 >= len(ss):
-		cs = 1
-	logmae[0] += abs(np.log(cr) - np.log(cs))
-logmae[0] = logmae[0] / m
 
 plt.xscale('log')
 plt.yscale('log')
@@ -180,8 +208,7 @@ plt.plot(ss, sn, 'bo', label='Sim')
 plt.xlabel(u'Size')
 plt.ylabel(u'Distribution')
 plt.legend(loc='upper right');  
-if not single:
-	filename = 'all'
+filename = 'all'
 plt.savefig(prefix+'SizeDistribution/'+str(filename)+'_cum.png', dpi=600)
 plt.cla()
 
@@ -190,108 +217,51 @@ rs = np.array(realsize)
 rn = np.array(realnum) * 1.0 / realsum
 ss = np.array(simsize)
 sn = np.array(simnum) * 1.0 / simsum
-'''
-m = max(max(rs), max(ss))
-pos1 = 0
-pos2 = 0
-for i in range(m):
-	temp = 0
-	while pos1 < len(rs) and rs[pos1] < i:
-		pos1 += 1
-	while pos2 < len(ss) and ss[pos2] < i:
-		pos2 += 1
-	if pos1 < len(rs) and rs[pos1] == i:
-		temp += np.log(rn[pos1]+1)
-	if pos2 < len(ss) and ss[pos2] == i:
-		temp -= np.log(sn[pos2]+1)
-	if temp != 0:
-		logmae[1] += abs(temp)
-'''
+ss1 = np.array(simsize1)
+sn1 = np.array(simnum1) * 1.0 / simsum1
+ss2 = np.array(simsize2)
+sn2 = np.array(simnum2) * 1.0 / simsum2
+ss3 = np.array(simsize3)
+sn3 = np.array(simnum3) * 1.0 / simsum3
+
 plt.xscale('log')
 plt.yscale('log')
-plt.plot(rs, rn, 'ro', label='Real')
-plt.plot(ss, sn, 'bo', label='Sim')
+plt.plot(rs, rn, '#ff6347', linestyle='o', label='Real')
+plt.plot(ss, sn, '#4876ff', label='Our framework')
+plt.plot(ss1, sn1, '#8c8c8c', label='BP')
+plt.plot(ss2, sn2, '#ffa500', label='EP')
+plt.plot(ss3, sn3, '#458b00', label='Base')
 plt.xlabel(u'Size')
 plt.ylabel(u'Distribution')
 plt.legend(loc='upper right');  
-if not single:
-	filename = 'all'
+filename = 'all_size'
 plt.savefig(prefix+'SizeDistribution/'+str(filename)+'_num.png', dpi=600)
 plt.cla()
 
-square = [0, 0]
-binrx, binry = GetBin(1.1, realsize, realcum) 
-binsx, binsy = GetBin(1.1, simsize, simcum)
-rs = np.array(binrx)
-rn = np.array(binry) * 1.0 / realsum
-ss = np.array(binsx)
-sn = np.array(binsy) * 1.0 / simsum
-
-m = len(ss)
-cr = 0
-cs = 0
-square = 0
-for i in range(m):
-	if i < len(rs):
-		cr = 1 - rn[i]
-	if i < len(ss):
-		cs = 1 - sn[i]
-	logmae[1] += abs(np.log(cr) - np.log(cs))
-	square += abs(np.log(cr) - np.log(cs)) * 1.1 ** (i+1)
-logmae[1] = logmae[1] / m
-square = square / m
-
-plt.xscale('log')
-plt.yscale('log')
-plt.plot(rs, rn, 'ro', label='Real')
-plt.plot(ss, sn, 'bo', label='Sim')
-plt.xlabel(u'Size')
-plt.ylabel(u'Distribution')
-plt.legend(loc='upper right');  
-if not single:
-	filename = 'all'
-plt.savefig(prefix+'SizeDistribution/'+str(filename)+'_cum_bin.png', dpi=600)
-plt.cla()
-
-binrx, binry = GetBin(1.1, realsize, realnum) 
-binsx, binsy = GetBin(1.1, simsize, simnum)
-rs = np.array(binrx)
-rn = np.array(binry) * 1.0 / realsum
-ss = np.array(binsx)
-sn = np.array(binsy) * 1.0 / simsum
-'''
-m = max(len(rs), len(ss))
-for i in range(m):
-	temp = 0
-	if i < len(rs):
-		temp += np.log(rn[i]+1)
-	if i < len(ss):
-		temp -= np.log(sn[i]+1)
-	if temp != 0:
-		logmae[3] += abs(temp)
-		square[1] += abs(temp) * 1.1 ** (i+1)
-'''
-plt.xscale('log')
-plt.yscale('log')
-plt.plot(rs, rn, 'ro', label='Real')
-plt.plot(ss, sn, 'bo', label='Sim')
-plt.xlabel(u'Size')
-plt.ylabel(u'Distribution')
-plt.legend(loc='upper right');  
-if not single:
-	filename = 'all'
-plt.savefig(prefix+'SizeDistribution/'+str(filename)+'_num_bin.png', dpi=600)
-plt.cla()
-
-fw = open(prefix+'BigSimCascades', 'w')
-for line in bigsim:
-	fw.write(line)
-fw.close()
-
-fw = open(prefix+'BigRealCascades', 'w')
-for line in bigreal:
-	fw.write(line)
-fw.close()
-
 #print logmae
 #print square
+rs = np.array(realsize)
+rn = np.array(realcum) * 1.0 / realsum
+ss = np.array(simsize)
+sn = np.array(simcum) * 1.0 / simsum
+ss1 = np.array(simsize1)
+sn1 = np.array(simcum1) * 1.0 / simsum1
+ss2 = np.array(simsize2)
+sn2 = np.array(simcum2) * 1.0 / simsum2
+ss3 = np.array(simsize3)
+sn3 = np.array(simcum3) * 1.0 / simsum3
+
+plt.xscale('log')
+plt.yscale('log')
+plt.plot(rs, rn, '#ff6347', linestyle='o', label='Real')
+plt.plot(ss, sn, '#4876ff', label='Our framework')
+plt.plot(ss1, sn1, '#8c8c8c', label='BP')
+plt.plot(ss2, sn2, '#ffa500', label='EP')
+plt.plot(ss3, sn3, '#458b00', label='Base')
+plt.xlabel(u'Size')
+plt.ylabel(u'Distribution')
+plt.legend(loc='upper right');  
+filename = 'all_size'
+plt.savefig(prefix+'SizeDistribution/'+str(filename)+'_num.png', dpi=600)
+plt.cla()
+
