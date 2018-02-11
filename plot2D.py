36d493c6fc726e3a040bd7f535143227493152a2
december <luyunfeicx@163.com>
2018-02-11 15:36:59 +0800

    fix bug

--- plot2D.py
+++ plot2D.py
@@ -96,7 +96,7 @@ def plot_by_x_y(x_list, y_list, xy_f_name):
 
     # pcolor(data)
     print data.max(), data.min()
-    pcolor(data, norm=colors.LogNorm(vmin=data.min(), vmax=data.max()), cmap='afhot')
+    pcolor(data, norm=colors.LogNorm(vmin=data.min(), vmax=data.max()), cmap='coolwarm')
     global First
     if First:
         colorbar()
