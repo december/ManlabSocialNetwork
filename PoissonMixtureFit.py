3b445ac4c257864b21dbdc20a252b6d04906ba04
luyunfei.louis <luyunfei.louis@bytedance.com>
2018-12-28 16:44:18 +0800

    poisson mixture model

--- PoissonMixtureFit.py
+++ PoissonMixtureFit.py
@@ -134,7 +134,7 @@ while i < n:
 			uid.append(userid)
 			allusers += 1
 		day = (posttime - ts) / 86400
-		#print day
+		print str(day) + ' ' + str(iddic[userid])
 		second = posttime % 86400
 		postlist[day][iddic[userid]].append(second)
 	i += size
