d761e2e280337e2befd33eeb9cd1c2a4b2b4df7e
luyunfei.louis <luyunfei.louis@bytedance.com>
2018-12-29 00:02:44 +0800

    debug

--- PoissonMixtureFit.py
+++ PoissonMixtureFit.py
@@ -81,8 +81,7 @@ def ObjLnPiQ(p, nm):
 		r += tf.reduce_sum(tf.gather(lnorderlist, kmatrix[j]))
 		r += scaler[j] * tf.cast(ietlist[j], tf.float64) * 10
 		tempr = tf.log(scaler[j]) + tf.log(tf.cast(ietlist[j], tf.float64))
-		tempk = tf.transpose(kmatrix)[j]
-		r -= tf.reduce_sum(tempr * tempk)
+		r -= tempr * tf.reduce_sum(kmatrix[j])
 	'''
 	before = list()
 	kmatrix = list()
