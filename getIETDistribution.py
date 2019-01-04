4c2ca66b01a23776cb04f71a5449505b4072826a
luyunfei.louis <luyunfei.louis@bytedance.com>
2019-01-04 23:55:51 +0800

    add baseline

--- getIETDistribution.py
+++ getIETDistribution.py
@@ -54,6 +54,9 @@ real_rt = {}
 sim = {}
 sim_post = {}
 sim_rt = {}
+base = {}
+base_post = {}
+base_rt = {}
 last_post = {}
 last_retweet = {}
 realdata = list()
@@ -116,15 +119,13 @@ if single:
 	namelist = os.listdir(prefix+str(filename)+'/')
 	position = prefix+str(filename)+'/'
 else:
-	namelist = os.listdir(prefix)
-	position = prefix
-	#namelist = os.listdir(prefix+'722911_twolevel_neighbor_cascades_simulation_10/')
-	#position = prefix+'722911_twolevel_neighbor_cascades_simulation_10/'	
-cnt = 0
+	#namelist = os.listdir(prefix)
+	#position = prefix
+	namelist = os.listdir(prefix+'722911_twolevel_neighbor_cascades_simulation_10/')
+	position = prefix+'722911_twolevel_neighbor_cascades_simulation_10/'	
 for name in namelist:
 	if not name.endswith('.detail'):
 		continue
-	cnt += 1
 	last_post = {}
 	last_retweet = {}
 	fr = open(position+name, 'r')
@@ -174,6 +175,67 @@ for name in namelist:
 				sim_rt[tempiet] = 1
 	fr.close()
 
+cnt = 1
+if single:
+	namelist = os.listdir(prefix+str(filename)+'/')
+	position = prefix+str(filename)+'/'
+else:
+	namelist = os.listdir(prefix)
+	position = prefix
+	#namelist = os.listdir(prefix+'722911_twolevel_neighbor_cascades_simulation_10/')
+	#position = prefix+'722911_twolevel_neighbor_cascades_simulation_10/'	
+for name in namelist:
+	if not name.endswith('.detail'):
+		continue
+	last_post = {}
+	last_retweet = {}
+	fr = open(position+name, 'r')
+	basedata = fr.readlines()
+	n = len(simdata)
+	i = 0
+	while i < n:
+		temp = basedata[i].split('\t')
+		number = int(temp[1]) + 1
+		timedic = {}
+		newtemp = basedata[i+1].split('\t')
+		if not last_post.has_key(newtemp[1]):
+			last_post[newtemp[1]] = list()
+		last_post[newtemp[1]].append(int(float(newtemp[2])))
+		for j in range(i+1, i+number):
+			info = basedata[j].split('\t')
+			tp = int(float(info[2]))
+			timedic[info[0]] = tp
+			if info[3] != '-1':
+				if not last_retweet.has_key(info[1]):
+					last_retweet[info[1]] = list()
+				last_retweet[info[1]].append(tp)
+				timedic[info[0]] = tp
+				tempiet = tp - timedic[info[3]]
+				if base.has_key(tempiet):
+					base[tempiet] += 1
+				else:
+					base[tempiet] = 1
+		i += number
+	for item in last_post:
+		last_post[item].sort()
+		m = len(last_post[item])
+		for i in range(m-1):
+			tempiet = last_post[item][i+1] - last_post[item][i]
+			if base_post.has_key(tempiet):
+				base_post[tempiet] += 1
+			else:
+				base_post[tempiet] = 1
+	for item in last_retweet:
+		last_retweet[item].sort()
+		m = len(last_retweet[item])
+		for i in range(m-1):
+			tempiet = last_retweet[item][i+1] - last_retweet[item][i]
+			if base_rt.has_key(tempiet):
+				base_rt[tempiet] += 1
+			else:
+				base_rt[tempiet] = 1
+	fr.close()
+
 realsize = sorted(real.keys())
 realnum = list()
 for size in realsize:
@@ -196,8 +258,20 @@ for i in range(n):
 	s -= simnum[i]
 	simcum.append(s)
 
+basesize = sorted(base.keys())
+basenum = list()
+for size in basesize:
+	basenum.append(base[size] * 1.0 / cnt)
+basecum = list()
+n = len(basenum)
+s = sum(basenum)
+for i in range(n):
+	s -= basenum[i]
+	basecum.append(s)	
+
 realsum = sum(realnum)
 simsum = sum(simnum)
+basesum = sum(basenum)
 
 start = 0
 
@@ -207,10 +281,13 @@ rn = np.array(realcum[start:]) * 1.0 / realsum
 ss = np.array(simsize[start:])
 sn = np.array(simcum[start:]) * 1.0 / simsum
 #plt.xlim(xmin=1000)
+bs = np.array(basesize[start:])
+bn = np.array(basecum[start:]) * 1.0 / basesum
 plt.xscale('log')
 plt.yscale('log')
 plt.plot(rs, rn, 'ro', label='Real', linewidth=2.5)
 plt.plot(ss, sn, 'b', label='Our method', linewidth=2.5)
+plt.plot(bs, bn, 'k', label='Poisson', linewidth=2.5)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 plt.xlabel(u'Response Time', fontsize=14)
@@ -243,9 +320,20 @@ for i in range(n):
 	s -= simnum[i]
 	simcum.append(s)
 
+basesize = sorted(base_post.keys())
+basenum = list()
+for size in basesize:
+	basenum.append(base_post[size] * 1.0 / cnt)
+basecum = list()
+n = len(basenum)
+s = sum(basenum)
+for i in range(n):
+	s -= basenum[i]
+	basecum.append(s)	
 
 realsum = sum(realnum)
 simsum = sum(simnum)
+basesum = sum(basenum)
 
 #binrx, binry = GetBin(1000, realsize, realcum) 
 #rs = np.array(binrx)
@@ -256,11 +344,14 @@ rn = np.array(realcum[start:]) * 1.0 / realsum
 rs, rn = GetBin(20000, rs, rn)
 ss = np.array(simsize[start:])
 sn = np.array(simcum[start:]) * 1.0 / simsum
+bs = np.array(basesize[start:])
+bn = np.array(basecum[start:]) * 1.0 / basesum
 #plt.xlim(xmin=1000)
 #plt.xscale('log')
 plt.yscale('log')
 plt.plot(rs, rn, 'ro', label='Real', linewidth=2.5)
 plt.plot(ss, sn, 'b', label='Our method', linewidth=2.5)
+plt.plot(bs, bn, 'k', label='Poisson', linewidth=2.5)
 plt.xticks(fontsize=14)
 plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 #plt.set_xticks([0, 20000, 40000, 60000, 80000])
@@ -296,8 +387,20 @@ for i in range(n):
 	s -= simnum[i]
 	simcum.append(s)
 
+basesize = sorted(base_rt.keys())
+basenum = list()
+for size in basesize:
+	basenum.append(base_rt[size] * 1.0 / cnt)
+basecum = list()
+n = len(basenum)
+s = sum(basenum)
+for i in range(n):
+	s -= basenum[i]
+	basecum.append(s)	
+
 realsum = sum(realnum)
 simsum = sum(simnum)
+basesum = sum(basenum)
 
 #binrx, binry = GetBin(1000, realsize, realcum) 
 #rs = np.array(binrx)
@@ -317,6 +420,7 @@ plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 plt.yticks(fontsize=14)
 plt.plot(rs, rn, 'ro', label='Real', linewidth=2.5)
 plt.plot(ss, sn, 'b', label='Our method', linewidth=2.5)
+plt.plot(bs, bn, 'k', label='Poisson', linewidth=2.5)
 plt.xlabel(u'Interevent time', fontsize=14)
 plt.ylabel(u'CDF', fontsize=14)
 plt.title('IET Distribution for Retweeting', fontsize=20)
